fac0dedbe0a294bd084f1f37f23b0d2ccfaf4621
yeojoy <yeojoy.kim@gmail.com>
2015-06-10 18:51:13 +0900

    Configuration 화면 개편
    부씨가 보내준 정보에 의하면 물의 양은 성별과 나이에 따라 달라진다. 그래서 기존
    키와 몸무게 화면은 지웠다. 그리고 여성일 경우 임신 중, 수유 중일 때 물의 양이
    늘어난다. 그래서 checkbox 화면을 추가했다. 하지만 checkbox의 경우 기존의
    listener는 안 되고 onclick으로 해야한다. 이 부분은 내일 바꿉시다.

--- waterManager/src/main/java/me/yeojoy/watermanager/WaterConfiguration.java
+++ waterManager/src/main/java/me/yeojoy/watermanager/WaterConfiguration.java
@@ -7,25 +7,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RemoteViews;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import me.yeojoy.watermanager.config.Consts;
-import me.yeojoy.watermanager.widget.WaterWidgetProvider;
 import me.yeojoy.watermanager.widget.WaterWidgetViewManager;
 import my.lib.MyLog;
 
 
 public class WaterConfiguration extends Activity implements Consts,
-        View.OnClickListener {
+        View.OnClickListener, CompoundButton.OnCheckedChangeListener {
     
     private static final String TAG = WaterConfiguration.class.getSimpleName();
     
     private int mAppWidgetId;
     private Context mContext;
-    
+
     private AppWidgetManager appWidgetManager;
 
     private Button mBtnFinish;
+
+    private RadioButton mRbMale, mRbFemale;
+    private EditText mEtUserAge;
+
+    private LinearLayout mLlForFemale;
+
+    private CheckBox mCbBeExpectingABaby, mCbFeedingABaby;
+
+    private TextView mTvResultDesc, mTvResult;
+
+    private int mQuantityOfWater = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +73,99 @@ public class WaterConfiguration extends Activity implements Consts,
 
         mBtnFinish = (Button) findViewById(R.id.btn_finish);
         mBtnFinish.setOnClickListener(this);
+
+        mRbMale = (RadioButton) findViewById(R.id.rb_male);
+        mRbFemale = (RadioButton) findViewById(R.id.rb_female);
+        mRbMale.setOnClickListener(mRadioButtonClickListener);
+        mRbFemale.setOnClickListener(mRadioButtonClickListener);
+
+        mEtUserAge = (EditText) findViewById(R.id.et_age);
+
+        mLlForFemale = (LinearLayout) findViewById(R.id.ll_for_female);
+
+        mCbBeExpectingABaby = (CheckBox) findViewById(R.id.cb_be_expecting_a_baby);
+        mCbFeedingABaby = (CheckBox) findViewById(R.id.cb_feeding_a_baby);
+        mCbBeExpectingABaby.setOnCheckedChangeListener(this);
+        mCbFeedingABaby.setOnCheckedChangeListener(this);
+
+        mTvResultDesc = (TextView) findViewById(R.id.tv_result_desc);
+        mTvResult = (TextView) findViewById(R.id.tv_result);
+
+    }
+
+    View.OnClickListener mRadioButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            boolean isChecked = ((RadioButton) view).isChecked();
+            int age = 0;
+            try {
+                age = Integer.parseInt(mEtUserAge.getText().toString());
+
+                if (age > 120 || age < 0) {
+                    Toast.makeText(mContext, R.string.warning_age_invalid,
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            } catch (NumberFormatException e) {
+                Toast.makeText(mContext, R.string.warning_input_age,
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            initQuantity();
+
+            if (isChecked) {
+                boolean isMale = false;
+                if (view.getId() == R.id.rb_male) {
+                    // 남자
+                    showViewForFemale(false);
+                    isMale = true;
+                } else {
+                    // 여자
+                    showViewForFemale(true);
+
+                }
+
+                showResult(age, isMale);
+            }
+
+        }
+    };
+
+    private void initQuantity() {
+        // CheckBox check 상태 초기화
+        mCbBeExpectingABaby.setOnCheckedChangeListener(null);
+        mCbFeedingABaby.setOnCheckedChangeListener(null);
+        mCbBeExpectingABaby.setChecked(false);
+        mCbFeedingABaby.setChecked(false);
+        // 물의 양 초기화
+        mQuantityOfWater = 0;
+
+        // Checkbox에 listener 등록
+        mCbBeExpectingABaby.setOnCheckedChangeListener(this);
+        mCbFeedingABaby.setOnCheckedChangeListener(this);
+
+        // 결과 화면 가림
+        mTvResult.setVisibility(View.GONE);
+        mTvResultDesc.setVisibility(View.GONE);
+
+    }
+
+    private void showViewForFemale(boolean b) {
+        if (b) {
+            mLlForFemale.setVisibility(View.VISIBLE);
+        } else {
+            mLlForFemale.setVisibility(View.GONE);
+        }
+    }
+
+    private void showResult(int age, boolean isMale) {
+        mTvResult.setVisibility(View.VISIBLE);
+        mTvResultDesc.setVisibility(View.VISIBLE);
+
+        setQuantityByAgeAndSex(age, isMale);
+
+        mTvResult.setText(String.format("%d ml", mQuantityOfWater));
     }
 
     @Override
@@ -87,6 +197,49 @@ public class WaterConfiguration extends Activity implements Consts,
             setResult(RESULT_OK, resultIntent);
             finish();
 
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (buttonView.getId() == R.id.cb_feeding_a_baby) {
+            if (isChecked)
+                mQuantityOfWater += 700;
+            else
+                mQuantityOfWater -= 700;
+
+        } else if (buttonView.getId() == R.id.cb_be_expecting_a_baby) {
+            if (isChecked)
+                mQuantityOfWater += 200;
+            else
+                mQuantityOfWater -= 200;
+        }
+    }
+
+
+    private void setQuantityByAgeAndSex(int age, boolean isMale) {
+        if (isMale) {
+            if (age < 1) mQuantityOfWater = 800;
+            else if (age < 3) mQuantityOfWater = 1100;
+            else if (age < 6) mQuantityOfWater = 1400;
+            else if (age < 9) mQuantityOfWater = 1800;
+            else if (age < 12) mQuantityOfWater = 2000;
+            else if (age < 15) mQuantityOfWater = 2300;
+            else if (age < 30) mQuantityOfWater = 2600;
+            else if (age < 50) mQuantityOfWater = 2500;
+            else if (age < 65) mQuantityOfWater = 2200;
+            else mQuantityOfWater = 2000;
+        } else {
+            if (age < 1) mQuantityOfWater = 800;
+            else if (age < 3) mQuantityOfWater = 1100;
+            else if (age < 6) mQuantityOfWater = 1400;
+            else if (age < 9) mQuantityOfWater = 1700;
+            else if (age < 12) mQuantityOfWater = 1800;
+            else if (age < 15) mQuantityOfWater = 2000;
+            else if (age < 30) mQuantityOfWater = 2100;
+            else if (age < 50) mQuantityOfWater = 1900;
+            else if (age < 65) mQuantityOfWater = 1800;
+            else mQuantityOfWater = 2000;
         }
     }
 }
